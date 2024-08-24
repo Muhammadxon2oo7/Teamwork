6dca0d76dcf8799d0be2a139db8fcfd8c1c3b312
Muhammadxon Toshpo'latov <110617834+Muhammadxon2oo7@users.noreply.github.com>
2024-08-24 15:31:05 +0500

    contact section finished

--- app.js
+++ app.js
@@ -5,3 +5,24 @@ hamburger.addEventListener("click",()=>{
 window.addEventListener("resize", function() {
     if (window.innerWidth >= 768) document.querySelector('.header__nav__wrapper').classList.remove("nav__display");
   });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const svgPaths = document.querySelectorAll("svg path");
+    const tooltip = document.getElementById('tooltip');
+
+    svgPaths.forEach(path => {
+        path.addEventListener('mouseenter', function() {
+            tooltip.innerHTML ='country name:'+'  '+ this.getAttribute('class'); // Tooltipga ma'lumotni joylash
+            tooltip.style.display = 'block';
+        });
+
+        path.addEventListener('mousemove', function(e) {
+            tooltip.style.top = `${e.pageY + 10}px`; // Tooltipni kursorning yuqorisida joylash
+            tooltip.style.left = `${e.pageX + 10}px`; // Tooltipni kursorning o'ng tomonida joylash
+        });
+
+        path.addEventListener('mouseleave', function() {
+            tooltip.style.display = 'none'; // Tooltipni yashirish
+        });
+    });
+});
